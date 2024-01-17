bdd1c3d6ef9d9a07fdf9263eca86effe5b4ccf1d
flurl <florian.st.klug@gmail.com>
2024-01-17 11:04:44 +0100

    Move the secret key into a explicit file
    For to not accidentally commit the secret key to a public repo move it
    to an untracked file. When deploying one has to create a new file.

--- epaper_converter/epaper_converter/settings.py
+++ epaper_converter/epaper_converter/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9#1&h86-+&l)t!8vnf91-11zbyz&@nmkkwwji_wthrw-2j-inm'
+with open(BASE_DIR / "epaper_converter/secret_key.txt") as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
