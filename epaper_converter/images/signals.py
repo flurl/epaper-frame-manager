1298490a91bac19822c08f55491f1d087e4a9b02
flurl <florian.st.klug@gmail.com>
2024-01-24 07:47:31 +0100

    Add new post_delete signal for images file deletion
    This signal handler deletes the image files from the filesystem whenever
    an image record is deleted.

---
+++ epaper_converter/images/signals.py
@@ -0,0 +1,9 @@
+from django.db.models.signals import post_delete
+from django.dispatch.dispatcher import receiver
+
+from .models import Image
+
+@receiver(post_delete, sender=Image)
+def on_image_delete(sender, instance, **kwargs):
+    instance.original_image.delete(False)
+    instance.converted_image.delete(False)
